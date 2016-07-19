fd1a813cd22e135171870a8e7487e18d30a5133d
Maciej Chałapuk <maciej@chalapuk.pl>
2016-07-19 19:08:13 +0200

    ~ moved context prototype building code to separate files

--- offensive.js
+++ offensive.js
@@ -1,17 +1,40 @@
 'use strict';
 
 var Assertion = require('./lib/classes/assertion').default;
-var AssertionWithArguments = require('./lib/classes/assertion-with-arguments').default;
 var Operator = require('./lib/classes/operator').default;
-var BinaryOperator = require('./lib/classes/binary-operator').default;
-var Alias = require('./lib/classes/alias').default;
 var ErrorBuilder = require('./lib/error-builder').default;
+
+var nodsl = require('./lib/nodsl');
 
 module.exports = check;
 
-module.exports.addAssertion = addAssertion;
-module.exports.addOperator = addOperator;
-module.exports.addNoop = addNoop;
+var assertionRegistry = require('./lib/registry/assertion');
+var operatorRegistry = require('./lib/registry/operator');
+var noopRegistry = require('./lib/registry/noop');
+
+// registering built-in assertions
+[
+  require('./lib/assertions/null'),
+  require('./lib/assertions/type'),
+  require('./lib/assertions/property'),
+  require('./lib/assertions/array'),
+]
+.forEach(function(assertions) {
+  for (var name in assertions) {
+    assertionRegistry.add(name, assertions[name]);
+  };
+});
+
+// registering built-in operators
+var operators = require('./lib/operators');
+
+Object.keys(operators).forEach(function(name) {
+  operatorRegistry.add(name, operators[name]);
+});
+
+// registering built-in noops
+require('./lib/noops')
+  .forEach(noopRegistry.add);
 
 // Object.setPrototypeOf polyfill
 if (!Object.setPrototypeOf) {
@@ -24,151 +47,11 @@ if (!Object.setPrototypeOf) {
   };
 }
 
-// contains all noop methods
-var noopProto = {};
-
-// registers a new assertion
-function addNoop(name) {
-  checkName(name);
-  Object.defineProperty(noopProto, name, {
-    get: returnThis,
-    enumerable: true,
-  });
-}
-
-function returnThis() {
-  return this;
-}
-
-// contains all assertion objects
-var assertionDefinitions = {};
-
-// contains all assertion methods
-var assertProto = Object.create(noopProto);
-
-// registers a new assertion
-function addAssertion(name, assertion) {
-  if (assertion instanceof Alias) {
-    addAssertion(name, getAliased(assertion));
-    return;
-  }
-
-  checkAssertionName(name);
-  checkAssertion(assertion);
-
-  assertionDefinitions[name] = assertion;
-
-  if (assertion instanceof AssertionWithArguments) {
-    Object.defineProperty(assertProto, name, {
-      value: function() { return this.add(name, assertion, [].slice.call(arguments)); },
-      enumerable: true,
-    });
-  } else {
-    Object.defineProperty(assertProto, name, {
-      get: function() { return this.add(name, assertion); },
-      enumerable: true,
-    });
-  }
-}
-
-// contains all operator objects
-var operatorDefinitions = {};
-
-// contains all operator methods
-var operatorProto = Object.create(noopProto);
-
-// registers a new assertion
-function addOperator(name, operator) {
-  if (operator instanceof Alias) {
-    addOperator(name, getAliased(operator));
-    return;
-  }
-
-  checkOperatorName(name);
-  checkOperator(operator);
-
-  operatorDefinitions[name] = operator;
-
-  // only binary operators in operatorProto
-  var proto = operator instanceof BinaryOperator? operatorProto: assertProto;
-
-  Object.defineProperty(proto, name, {
-    get: function() { return this.add(name, operator); },
-    enumerable: true,
-  });
-}
-
-// this must work when no assertions are added yet,
-// so checks are implemented in vanilla javascript
-function checkAssertionName(name) {
-  checkName(name);
-  if (name in assertionDefinitions) {
-    throw new Error('assertion of name '+ name +' already registered');
-  }
-}
-function checkName(name) {
-  if (typeof name !== 'string') {
-    throw new Error('name must be a string; got '+ name);
-  }
-}
-function checkAssertion(assertion) {
-  if (!(assertion instanceof Assertion)) {
-    throw new Error('assertion must be an instance of Assertion');
-  }
-}
-function checkOperatorName(name) {
-  checkName(name);
-  if (name in operatorDefinitions) {
-    throw new Error('operator of name '+ name +' already registered');
-  }
-}
-function checkOperator(operator) {
-  if (!(operator instanceof Operator)) {
-    throw new Error('operator must be an instance of Operator');
-  }
-}
-
-function getAliased(assertion) {
-  var aliased = assertionDefinitions[assertion.aliasFor];
-  if (aliased) {
-    return aliased;
-  }
-  aliased = operatorDefinitions[assertion.aliasFor];
-  if (aliased) {
-    return aliased;
-  }
-  throw new Error('no assertion assertion or operator of name '+ assertion.aliasFor +' registered');
-}
-
-var nullAssertions = require('./lib/assertions/null');
-var typeAssertions = require('./lib/assertions/type');
-var propertyAssertions = require('./lib/assertions/property');
-var arrayAssertions = require('./lib/assertions/array');
-
-[
-  nullAssertions, typeAssertions, propertyAssertions, arrayAssertions,
-]
-.forEach(function(assertions) {
-  Object.keys(assertions).forEach(function(name) {
-    addAssertion(name, assertions[name]);
-  });
-});
-
-var builtInOperators = require('./lib/operators');
-
-Object.keys(builtInOperators).forEach(function(name) {
-  addOperator(name, builtInOperators[name]);
-});
-
-var builtInNoops = require('./lib/noops');
-
-builtInNoops.forEach(addNoop);
-
 // exported check function
 function check(value, name) {
-  checkName(name);
+  nodsl.check(typeof name === 'string', 'name must be a string; got ', name);
 
-  var context = Object.create(assertProto);
+  var context = Object.create(assertionRegistry.contextProto);
   context.value = value;
   context.name = name;
   context.add = add;
@@ -179,7 +62,7 @@ function check(value, name) {
   };
   operatorContext.value = value;
   operatorContext.add = runOperator;
-  Object.setPrototypeOf(operatorContext, operatorProto);
+  Object.setPrototypeOf(operatorContext, operatorRegistry.contextProto);
 
   var priv = {};
   priv.state = new State();
