8c4dd0e1737de9691c0ffa0bd8589f88b05dd4ab
Maciej Chałapuk <maciej@chalapuk.pl>
2018-10-29 23:56:55 +0100

    + call operator fobidden in inner checks

--- src/ContextFactory.ts
+++ src/ContextFactory.ts
@@ -6,24 +6,27 @@ import ObjectSerializer from './ObjectSerializer';
 
 const serializer = new ObjectSerializer();
 
+export interface ContextConstructor {
+  new (testedValue : any, varName : string) : AssertionContext<any>;
+  prototype : any;
+}
+
 /**
  * @author Maciej Chałapuk (maciej@chalapuk.pl)
  */
 export class ContextFactory {
-  private readonly ContextConstructor : {
-    new (testedValue : any, varName : string, operators : any) : AssertionContext<any>;
-    prototype : any;
-  };
+  private readonly Constructor : ContextConstructor;
+  private readonly InnerConstructor : ContextConstructor;
 
   constructor(
     private readonly assertions : object,
     private readonly operators : object,
   ) {
-    const check = this.create.bind(this);
+    const innerCheck = this.createInner.bind(this);
 
     // Copy the ContextImpl class in order to be able to set its prototype
     // to different object in each instance of the factory.
-    this.ContextConstructor = function ContextConstructor(
+    this.Constructor = function ContextConstructor(
       this : RuntimeContext,
       testedValue : any,
       varName : string,
@@ -43,15 +46,35 @@ export class ContextFactory {
       }
       Object.setPrototypeOf(operatorContext, operators);
 
-      ContextImpl.call(self, testedValue, varName, operatorContext, check);
+      ContextImpl.call(self, testedValue, varName, operatorContext, innerCheck);
     } as any;
 
-    this.ContextConstructor.prototype = { ...ContextImpl.prototype };
-    Object.setPrototypeOf(this.ContextConstructor.prototype, assertions);
+    this.Constructor.prototype = { ...ContextImpl.prototype };
+    Object.setPrototypeOf(this.Constructor.prototype, assertions);
+
+    // Different constructor for inner checks in order to forbid invoking call operator.
+    this.InnerConstructor = function InnerContextConstructor(
+      this : RuntimeContext,
+      testedValue : any,
+      varName : string,
+    ) {
+      function innerOperatorContext<T>() : T {
+        throw new Error(`invoking call operator inside inner check is forbidden (${varName})`);
+      }
+      Object.setPrototypeOf(innerOperatorContext, operators);
+      ContextImpl.call(this, testedValue, varName, innerOperatorContext, innerCheck);
+    } as any;
+
+    this.InnerConstructor.prototype = this.Constructor.prototype;
   }
 
   create<T>(testedValue : T, varName : string) : AssertionContext<T> {
-    const context = new this.ContextConstructor(testedValue, varName, this.operators);
+    const context = new this.Constructor(testedValue, varName);
+    return context as AssertionContext<T>;
+  }
+
+  private createInner<T>(testedValue : T, varName : string) : AssertionContext<T> {
+    const context = new this.InnerConstructor(testedValue, varName);
     return context as AssertionContext<T>;
   }
 }
