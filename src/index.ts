d645736ab69a4d14c1ec25d0904e8302a872db86
Maciej Chałapuk <maciej@chalapuk.pl>
2020-04-12 22:58:11 +0200

    ~ fixed return value of withError(...)

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,23 @@ import { CheckFunction } from './model';
 import './operators/register'
 import './connectors/register'
 
-const factories : { [_ : string] : CheckFunction } = {};
+const factories : { [_ : string] : { check: CheckFunction } } = {};
 
-export const check = withError('ContractError');
+export const check = withError('ContractError').check;
 export default check;
 
 /**
  * @author Maciej Chałapuk (maciej@chalapuk.pl)
  */
-export function withError(errorName : string) : CheckFunction {
+export function withError(errorName : string) : { check: CheckFunction } {
   return factories[errorName] || (() => {
     const { assertions, operators } = Registry.instance.contextProto;
     const factory = new BuilderFactory(assertions, operators, errorName);
 
-    return factories[errorName] = function check<T>(testedValue : T, varName : string) {
-      return factory.create<T>(testedValue, varName);
+    return factories[errorName] = {
+      check: function check<T>(testedValue : T, varName : string) {
+        return factory.create<T>(testedValue, varName);
+      },
     };
   })()
 }
